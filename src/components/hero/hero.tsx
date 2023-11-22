8d83fb9785d5cdc6647163fe88ba1998eabab6b3
mhimranhossain <mh.imranhossain@outlook.com>
2023-11-22 09:55:45 +0600

    Update 11/22/2023 09:55:45

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -13,27 +13,27 @@ export const Hero = component$(() => {
             alt="hero"
             width="400"
             height="550"
-            class="border max-w-sm rounded-lg shadow-2xl shadow-black  "
+            class=" max-w-sm rounded-lg shadow-2xl shadow-black  animate-fade-right animate-duration-1000 animate-delay-150 animate-ease-in-out animate-normal animate-fill-both"
           />
-        <div class="max-w-sm ">
+        <div class="max-w-sm overflow-hidden">
           <h1 class="text-7xl text-center lg:text-9xl lg:text-left  ">
-            <section class=" text-transparent bg-clip-text bg-gradient-to-b from-gray-100 to-gray-600">
+            <section class="animate-fade-down animate-delay-75 text-transparent bg-clip-text bg-gradient-to-b from-gray-100 to-gray-600">
               YOUR
             </section>
-            <section class=" text-transparent bg-clip-text bg-gradient-to-b from-gray-100 to-gray-600">
+            <section class="animate-fade-down animate-delay-150 text-transparent bg-clip-text bg-gradient-to-b from-gray-100 to-gray-600">
               FITNESS
             </section>
-            <section class=" text-transparent bg-clip-text bg-gradient-to-b from-red-600 to-red-900">
+            <section class="animate-fade-down animate-delay-200 text-transparent bg-clip-text bg-gradient-to-b from-red-600 to-red-900">
               JOURNEY
             </section>
           </h1>
-          <p class="text-center lg:text-left py-6 text-2xl">
+          <p class="text-center lg:text-left py-6 text-2xl animate-fade-left animate-delay-500">
             Starts today. <br />
             Personal training <br />
             with Zafir Anam.
           </p>
           <div class=" flex justify-center lg:justify-start">
-            <button class="btn btn-outline btn-primary">LET'S GO</button>
+            <button class="btn btn-outline btn-primary animate-fade-left animate-delay-500">LET'S GO</button>
           </div>
         </div>
       </div>
--- src/components/hero/heroMid.tsx
+++ src/components/hero/heroMid.tsx
@@ -1,10 +1,18 @@
-import { component$ } from "@builder.io/qwik"
+import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik"
+import styles from "./heroMid.css?inline"
 
 export const HeroMid = component$(() => {
+  useStylesScoped$(styles)
+  const header = useSignal<HTMLHeadingElement>()
+  
+
+  
   return (
-    <div class="py-10  overflow-scroll m-scroll relative overflow-x-scroll">
-      <h1 class=" text-9xl font-bold whitespace-nowrap text-stroke text-transparent m-scroll__title">
-        WEIGHT LOSS / MUSCLE GAIN / HEATHLY LIFESTYLE
+    <div class=" py-10  m-scroll overflow-hidden">
+      <h1
+        ref={header}
+        class="  text-6xl lg:text-9xl font-bold whitespace-nowrap text-stroke text-transparent m-scroll__title">
+        WEIGHT LOSS / MUSCLE GAIN / HEATHLY LIFESTYLE /
       </h1>
     </div>
   )
