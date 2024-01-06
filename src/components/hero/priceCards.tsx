448451b88aec54ca1feac64e7d1cf10091153cf3
mhimranhossain <mh.imranhossain@outlook.com>
2024-01-06 11:19:09 +0600

    pricing cards

--- src/components/hero/priceCards.tsx
+++ src/components/hero/priceCards.tsx
@@ -1,59 +1,7 @@
 import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik"
 
-const packages = {
-  "01": {
-    title: "40-Day Fitness Program",
-    price: "990 BDT",
-    details: [
-      "Program Duration: 40 Days",
-      "Customized diet plan",
-      "Customized home workout",
-      "Tailored for Fat loss/Weight loss/Weight gain",
-      "Building the habit of consistency",
-      "Flexible & affordable nutrition program",
-      "Bi-weekly diet & training adjustments after closely monitoring progress",
-    ],
-  },
-  "02": {
-    title: "Premium Coaching - 4 Weeks",
-    price: "1990 BDT",
-    details: [
-      "Program Duration: 4 Weeks",
-      "Customized diet plan",
-      "Customized Gym/Home workout",
-      "Tailored for Muscle build/Bulk up/Fat loss/Weight loss/Six-pack",
-      "Flexible & affordable nutrition program",
-      "Weekly diet & training adjustments after closely monitoring performance",
-      "Supplementation/lifestyle guide",
-    ],
-  },
-  "03": {
-    title: "Premium Coaching - 12 Weeks",
-    price: "3490 BDT",
-    details: [
-      "Program Duration: 12 Weeks",
-      "Customized diet plan",
-      "Customized Gym/Home workout",
-      "Tailored for Muscle build/Bulk up/Fat loss/Weight loss/Six-pack",
-      "Flexible & affordable nutrition program",
-      "Weekly diet & training adjustments after closely monitoring performance",
-      "Supplementation/lifestyle guide",
-    ],
-  },
-  "04": {
-    title: "Premium Coaching - 24 Weeks",
-    price: "5450 BDT",
-    details: [
-      "Program Duration: 24 Weeks",
-      "Customized diet plan",
-      "Customized Gym/Home workout",
-      "Tailored for Muscle build/Bulk up/Fat loss/Weight loss/Six-pack",
-      "Flexible & affordable nutrition program",
-      "Weekly diet & training adjustments after closely monitoring performance",
-      "Supplementation/lifestyle guide",
-    ],
-  },
-}
+import { packages } from "~/data/packages"  
+
 export const PriceCards = component$(() => {
   const animate = useSignal(false)
 
@@ -67,51 +15,45 @@ export const PriceCards = component$(() => {
         {Object.entries(packages).map(
           ([packageNumber, { title, price, details }]) => (
             <div
-              class={`${
-                animate.value &&
+              class={`${animate.value &&
                 "animate-fade-right animate-duration-1000 animate-delay-300 ease-in-out"
-              } card bg-base-300 shadow-3xl shadow-black/50`}
+                } card bg-base-300 shadow-3xl shadow-black/50`}
               key={packageNumber}>
               <div class="card-body">
                 <h2
-                  class={`${
-                    animate.value &&
+                  class={`${animate.value &&
                     "animate-fade-right animate-duration-1000 animate-delay-500 ease-in-out"
-                  } card-title`}>
+                    } card-title`}>
                   Package {packageNumber}
                 </h2>
                 <p
-                  class={`${
-                    animate.value &&
+                  class={`${animate.value &&
                     "animate-fade-right animate-duration-1000 animate-delay-700 ease-in-out"
-                  } text-base-content text-opacity-40`}>
+                    } text-base-content text-opacity-40`}>
                   {title}
                 </p>
                 <p
-                  class={`${
-                    animate.value &&
+                  class={`${animate.value &&
                     "animate-fade-right animate-duration-1000 animate-delay-1000 ease-in-out"
-                  } text-3xl font-bold`}>
+                    } text-3xl font-bold`}>
                   {price}
                 </p>
                 <div class="card-actions justify-end">
                   <button
-                    class={`${
-                      animate.value &&
+                    class={`${animate.value &&
                       "animate-fade-left animate-duration-1000 animate-delay-1000 ease-in-out"
-                    } btn btn-primary btn-outline`}>
+                      } btn btn-primary btn-outline`}>
                     Buy Now
                   </button>
                 </div>
               </div>
               <ul class="menu p-4 shadow-lg bg-base-200 rounded-box">
-                {details.map((detail, index) => (
+                {details.map((detail: string, index: number) => (
                   <li key={index}>
                     <p
-                      class={`${
-                        animate.value &&
+                      class={`${animate.value &&
                         "animate-fade-left animate-duration-1000 animate-delay-1000 ease-in-out"
-                      }`}>
+                        }`}>
                       {detail}
                     </p>
                   </li>
---
+++ src/data/packages.ts
@@ -0,0 +1,41 @@
+export const packages = {
+	"01": {
+		title: "Premium online coaching",
+		price: "2490 BDT",
+		details: [
+			"Program Duration: 4 Weeks",
+			"Customized diet plan",
+			"Customized Gym/Home workout",
+			"Tailored for Muscle build/bulk up/ fat loss/weight loss/six pack",
+			"Flexible & affordable nutrition program",
+			"Weekly diet & training adjustments After closely monitoring performance.",
+			"Supplementation/lifestyle guide",
+		],
+	},
+	"02": {
+		title: "Premium Online Coaching",
+		price: "4990 BDT",
+		details: [
+			"Program Duration: 12 Weeks",
+			"Customized diet plan",
+			"Customized Gym/Home workout",
+			"Tailored for Muscle build/bulk up/ fat loss/weight loss/six pack",
+			"Flexible & affordable nutrition program",
+			"Weekly diet & training adjustments After closely monitoring performance.",
+			"Supplementation/lifestyle guide",
+		],
+	},
+	"03": {
+		title: "Premium Online Coaching",
+		price: "7450 BDT",
+		details: [
+			"Program Duration: 24 Weeks",
+			"Customized diet plan",
+			"Customized Gym/Home workout",
+			"Tailored for Muscle build/bulk up/ fat loss/weight loss/six pack",
+			"Flexible & affordable nutrition program",
+			"Weekly diet & training adjustments After closely monitoring performance.",
+			"Supplementation/lifestyle guide",
+		],
+	}
+}
